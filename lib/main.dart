76780991581bc1ab60e31cb0c1e46718234089dd
ik-top-digital <top.digital.ik@gmail.com>
2022-05-07 19:39:26 +0300

    step 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,135 @@
 import 'package:flutter/material.dart';
 
 void main() {
-runApp(const MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-const MyApp({Key? key}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
 // This widget is the root of your application.
-@override
-Widget build(BuildContext context) {
-	return MaterialApp(
-	title: 'Flutter Demo',
-	theme: ThemeData(
-		primarySwatch: Colors.blue,
-	),
-	home: const MyHomePage(),
-	);
-}
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
 }
 
 class MyHomePage extends StatefulWidget {
-const MyHomePage({Key? key}) : super(key: key);
+  const MyHomePage({Key? key}) : super(key: key);
 
-@override
-_MyHomePageState createState() => _MyHomePageState();
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int _activeCurrentStep = 0;
+
+  TextEditingController name = TextEditingController();
+  TextEditingController email = TextEditingController();
+  TextEditingController pass = TextEditingController();
+  TextEditingController address = TextEditingController();
+  TextEditingController pincode = TextEditingController();
 
 // Here we have created list of steps that
 // are required to complete the form
-List<Step> stepList() => [
-		const Step(title: Text('Account'), content: Center(child: Text('Account'),)),
-		const Step(title: Text('Address'), content: Center(child: Text('Address'),)),
-		const Step(title: Text('Confirm'), content: Center(child: Text('Confirm'),))
-];
-@override
-Widget build(BuildContext context) {
-	return Scaffold(
-	appBar: AppBar(
-		centerTitle: true,
-		backgroundColor: Colors.green,
-		title: const Text('Multi Step Form',style: TextStyle(color: Colors.white), ),
-	),
-	
-	// Here we have initialized the stepper widget
-	body: Stepper(
-		steps: stepList(),
-	)
-	);
-}
+  List<Step> stepList() => [
+        Step(
+          state:
+              _activeCurrentStep <= 0 ? StepState.editing : StepState.complete,
+          isActive: _activeCurrentStep >= 0,
+          title: const Text('Account'),
+          content: Container(
+            child: Column(
+              children: [
+                TextField(
+                  controller: name,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Full Name',
+                  ),
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                TextField(
+                  controller: email,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Email',
+                  ),
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                TextField(
+                  controller: pass,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        const Step(
+            title: Text('Address'),
+            content: Center(
+              child: Text('Address'),
+            )),
+        const Step(
+            title: Text('Confirm'),
+            content: Center(
+              child: Text('Confirm'),
+            ))
+      ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: Colors.green,
+        title: const Text(
+          'Multi Step Form',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+
+      // Here we have initialized the stepper widget
+      body: Stepper(
+        type: StepperType.horizontal,
+        currentStep: _activeCurrentStep,
+        steps: stepList(),
+        onStepContinue: () {
+          if (_activeCurrentStep < (stepList().length - 1)) {
+            setState(() {
+              _activeCurrentStep += 1;
+            });
+          }
+        },
+        onStepCancel: () {
+          if (_activeCurrentStep == 0) {
+            return;
+          }
+
+          setState(() {
+            _activeCurrentStep -= 1;
+          });
+        },
+        onStepTapped: (int index) {
+          setState(() {
+            _activeCurrentStep = index;
+          });
+        },
+      ),
+    );
+  }
 }
